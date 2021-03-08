276f3ead1f35bd5b483b20ac1697ec9ca0f143a9
Itouch-up <S20026@gsm.hs.kr>
2021-03-08 17:08:16 +0900

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello'
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
