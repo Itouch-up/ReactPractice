4d93f5dd6238d7c928b799252f7098117e48364c
Itouch-up <S20026@gsm.hs.kr>
2021-03-09 23:09:06 +0900

    210309

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
-import React from 'react';
-import InputSample from './InputSample';
+import React,{useRef,useState} from 'react';
+import CreateUser from './CreateUser';
+import UserList from './UserList';
 
 function App() {
+  const [inputs, setInputs]=useState({
+    username:'',
+    email:'',
+  })
+  const {username,email}=inputs
+  const onChange = e =>{
+    const {name,value} = e.target
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+
+  const [users,setUsers] = useState([
+      {
+          id:1,
+          username: 'touchup',
+          email: 'touchup1479@gmail.com'
+      },
+      {
+          id:2,
+          username: 'tester',
+          email: 'test@example.com'
+      },
+      {
+          id:3,
+          username: 'liz',
+          email: 'liz@example.com'
+      }
+  ])
+
+  
+
+  const nextId = useRef(4)
+
+  const onCreate = () => {
+    const user={
+      id:nextId.current,
+      username,
+      email,
+    }
+    setUsers([/*...users,user*/users.concat(user)])
+    setInputs({
+      username:'',
+      email:''
+    })
+
+    console.log(nextId.current)
+    nextId.current += 1
+  }
+
   return (
-    <InputSample />
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
+      <UserList users={users}/>
+    </>
   )
 }
 
--- src/InputSample.js
+++ src/InputSample.js
@@ -1,22 +1,30 @@
 import React,{useState} from 'react'
 
 function InputSample() {
-    const [text, setText] = useState('')
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: '',
+    })
+    const {name,nickname} = inputs
     const onChange = (e) => {
+        const {name,value} = e.target
         
-        setText(e.target.value)
-        
+        setInputs({...inputs,
+            [name]: value,})
     }
-    const onReset = () =>{setText('')}
+    const onReset = () =>{setInputs({
+        name:'',
+        nickname:'',
+    })}
     return(
         <div>
-            <input onChange={onChange} value={text} />
-            <button>
-                초기화
+            <input name="name" placeholder="이름" onChange={onChange} value={name}/>
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
+            <button onClick={onReset}>초기화
             </button>
             <div>
                 <b>값 : </b>
-                {text}
+                {name} ({nickname})
             </div>
         </div>
     )
