79484a66ab9c0c501475a471ee61d89fa2c29f87
Itouch-up <S20026@gsm.hs.kr>
2021-03-15 21:07:18 +0900

    210315

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function reducer(state,action){
         throw new Error('Unhandled action')
   }
 }
-export const UserDispatch = creacteContext(null)
+export const UserDispatch = createContext(null)
 function App() {
   const [state,dispatch] = useReducer(reducer,initialState)
   const [form,onChange,reset] = useInputs({
@@ -78,26 +78,13 @@ function App() {
     reset()
   },[username,email,reset])
 
-  const onToggle = useCallback(id=>{
-    dispatch({
-      type:'TOGGLE_USER',
-      id
-    })
-  },[])
-
-  const onRemove = useCallback(id=>{
-    dispatch({
-      type:'REMOVE_USER',
-      id
-    })
-  },[])
 
   const count = useMemo(() => countActiveUsers(users),[users])
 
   return (
     <UserDispatch.Provider value={dispatch}>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-      <UserList users={users} onToggle={onToggle} onRemove={onRemove}/>
+      <UserList users={users}/>
       <div>활성 사용자 수: {count}</div>
     </UserDispatch.Provider>
   )
