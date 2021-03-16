9c8800c4c72533f11152a1e07ff360a5fb271b85
Itouch-up <S20026@gsm.hs.kr>
2021-03-16 21:08:26 +0900

    210316

--- src/App.js
+++ src/App.js
@@ -55,35 +55,14 @@ function reducer(state,action){
 export const UserDispatch = createContext(null)
 function App() {
   const [state,dispatch] = useReducer(reducer,initialState)
-  const [form,onChange,reset] = useInputs({
-    username:'',
-    email:''
-  })
-  const {username,email} = form;
-  const nextID = useRef(4)
+
   const{users}=state
-
-  
-
-  const onCreate = useCallback(()=>{
-    dispatch({
-      type:'CREATE_USER',
-      user:{
-        id:nextID.current,
-        username,
-        email
-      }
-    })
-    nextID.current+=1
-    reset()
-  },[username,email,reset])
-
 
   const count = useMemo(() => countActiveUsers(users),[users])
 
   return (
     <UserDispatch.Provider value={dispatch}>
-      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
+      <CreateUser />
       <UserList users={users}/>
       <div>활성 사용자 수: {count}</div>
     </UserDispatch.Provider>
