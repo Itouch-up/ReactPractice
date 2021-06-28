7ae3b82d557f3a4633f0af0666a578b6b7c9d550
Itouch-up <zxcsun1479@outlook.kr>
2021-06-28 19:59:48 +0900

    210628

--- src/Counter.js
+++ src/Counter.js
@@ -33,17 +33,32 @@ import React,{Component} from 'react'
 // }
 class Counter extends Component{
 
-    handleIncrease(){
-        console.log('increase')
+    // constructor(props){
+    //     super(props)
+    //     this.handleIncrease=this.handleIncrease.bind(this)
+    //     this.handleDecrease=this.handleDecrease.bind(this)
+    // }
+    constructor(props){
+        super(props)
+        this.state={
+            counter:0
+        }
     }
-    handleDecrease(){
-        console.log('decrease')
+    handleIncrease=()=>{
+        this.setState({
+            counter:this.state.counter+1
+        })
+    }
+    handleDecrease=()=>{
+        this.setState({
+            counter:this.state.counter-1
+        })
     }
 
     render(){
         return(
             <div>
-                <h1>0</h1>
+                <h1>{this.state.counter}</h1>
                 <button onClick={this.handleIncrease}>+1</button>
                 <button onClick={this.handleDecrease}>-1</button>
             </div>
